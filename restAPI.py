0679865d6853ab8b22ed49bac1a591b510111e08
Piotr <pm@piotrmichna.pl>
2021-04-18 18:35:20 +0200

    define setter method to parse api_url

--- restAPI.py
+++ restAPI.py
@@ -15,3 +15,25 @@ class RestApi(object):
                 self.endpoint = None
         else:
             self.endpoint = None
+
+    def set_api_url(self, url=None):
+        if url is None:
+            self.api_url = self.API_URL
+        else:
+            url_arr = str(url).split('/')
+            if len(url_arr) > 1:
+                if str(url_arr[0]).lower() == 'http:' or str(url_arr[0]).lower() == 'https:':
+                    if str(url_arr[0]).lower() == 'http:':
+                        ht = 'http://'
+                    else:
+                        ht = 'https://'
+                    for n, val in enumerate(url_arr):
+                        if str(val) == '':
+                            url_arr.pop(n)
+                    url_arr.pop(0)
+
+                    self.api_url = ht + '/'.join(url_arr) + '/'
+                else:
+                    self.api_url = None
+            else:
+                self.api_url = None
