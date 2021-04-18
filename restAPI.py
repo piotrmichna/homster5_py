3a33f5fed7df5c44c798e86423dc7a86f73258d4
Piotr <pm@piotrmichna.pl>
2021-04-18 18:32:58 +0200

    define setter method to parse endpoint

--- restAPI.py
+++ restAPI.py
@@ -4,3 +4,14 @@ class RestApi(object):
     def __init__(self, endpoint=None):
         self.endpoint = None
         self.api_url = None
+        self.set_end_point(endpoint)
+
+    def set_end_point(self, endpoint=None):
+        if endpoint:
+            endpoint_arr = str(endpoint).split('/')
+            if len(endpoint_arr):
+                self.endpoint = '/'.join(endpoint_arr) + '/'
+            else:
+                self.endpoint = None
+        else:
+            self.endpoint = None
