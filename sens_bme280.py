65fa01f1f17f96fe24c3488202ad8f5b9b580d08
Piotr <pm@piotrmichna.pl>
2021-04-19 13:54:32 +0200

    init parse atribute in Bme280Sensor class

--- sens_bme280.py
+++ sens_bme280.py
@@ -7,10 +7,14 @@ class Bme280Sensor(object):
     ADDRESS = 0x76
 
     def __init__(self, adr=None, port=None):
-        if adr is not None:
-            self.ADDRESS = 0x76
-        if port is not None:
+        if adr is not None and '0x' in adr:
+            self.ADDRESS = adr
+        else:
+            self.ADDRESS = Bme280Sensor.ADDRESS
+        if port is not None and type(port) is int:
             self.PORT = port
+        else:
+            self.PORT = Bme280Sensor.PORT
         self.buss = smbus2.SMBus(self.PORT)
         self.calibration_params = bme280.load_calibration_params(self.buss, self.ADDRESS)
         self.data = None
@@ -27,7 +31,7 @@ class Bme280Sensor(object):
         print(f"    data pomiaru: {self.data.timestamp}")
         print(f'     temperatura: {round(self.data.temperature, 2)}')
         print(f'       ciśnienie: {round(self.data.pressure)}')
-        print(f"      wilgotność: {round(self.data.humidity,1)}")
+        print(f"      wilgotność: {round(self.data.humidity, 1)}")
 
 
 if __name__ == '__main__':
